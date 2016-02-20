c3c2d73f6bb5e46f966c8049845d3dad9c9cb933
Hurkapburk <johbe770@gmail.com>
2016-02-20 12:19:03 +0100

    Draw Circles and display info done

--- app/src/main/java/com/hurk/da569a_lab2/ScreenInfo.java
+++ app/src/main/java/com/hurk/da569a_lab2/ScreenInfo.java
@@ -39,13 +39,18 @@ public class ScreenInfo extends Activity {
             super.onDraw(canvas);
 
             canvas.drawARGB(255, 154, 246, 240);
+            int width = canvas.getWidth();
+            int height = canvas.getHeight();
             Paint paint = new Paint();
             paint.setColor(Color.BLACK);
-            paint.setTextSize(30f);
-            int width = canvas.getWidth();
-            int heigth = canvas.getHeight();
+            paint.setTextSize(height / 40);
+            Paint paintInfo = new Paint();
+            paintInfo.setColor(Color.BLACK);
+            paintInfo.setTextSize(height / 20);
 
-            int x = 10, y = 30, step = 36;
+            int x = width / 10, y = height / 2, step = 60;
+            canvas.drawText("Screen Info: ", x, y, paintInfo);
+            y = y + 2 * step;
             canvas.drawText("Density: " + displayMetrics.density, x, y, paint);
             y = y + step;
             canvas.drawText("Scaled density: " + displayMetrics.scaledDensity, x, y, paint);
@@ -54,7 +59,7 @@ public class ScreenInfo extends Activity {
             y = y + step;
             canvas.drawText("Width: " + width, x, y, paint);
             y = y + step;
-            canvas.drawText("Height: " + heigth, x, y, paint);
+            canvas.drawText("Height: " + height, x, y, paint);
             y = y + step;
             canvas.drawText("X dpi: " + displayMetrics.xdpi, x, y, paint);
             y = y + step;
